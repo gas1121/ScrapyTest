e21a3e5cdd45552d6593ce53a9c543551bbbbc27
gas1121 <jtdzhx@gmail.com>
2017-02-10 11:10:31 +0800

    add scrapy download middleware for selenium.

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -52,9 +52,10 @@ ROBOTSTXT_OBEY = True
 
 # Enable or disable downloader middlewares
 # See http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    'tutorial.middlewares.MyCustomDownloaderMiddleware': 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+    'tutorial.middlewares.middleware.SeleniumDownloaderMiddleware': 543,
+    'scrapy.downloadermiddlewares.useragent.UserAgentMiddleware': None
+}
 
 # Enable or disable extensions
 # See http://scrapy.readthedocs.org/en/latest/topics/extensions.html
--- tutorial/spiders/toho_spider.py
+++ tutorial/spiders/toho_spider.py
@@ -3,8 +3,8 @@ import scrapy
 
 class QuotesSpider(scrapy.Spider):
     name = "toho"
-    # start_urls = ['https://hlo.tohotheater.jp/net/schedule/076/TNPI2000J01.do']
-    start_urls = ['https://hlo.tohotheater.jp/net/ticket/076/TNPI2040J03.do?site_cd=076&jyoei_date=20170209&gekijyo_cd=0761&screen_cd=10&sakuhin_cd=014183&pf_no=5&fnc=1&pageid=2000J01&enter_kbn=']
+    start_urls = ['https://hlo.tohotheater.jp/net/schedule/076/TNPI2000J01.do']
+    #start_urls = ['https://hlo.tohotheater.jp/net/ticket/076/TNPI2040J03.do?site_cd=076&jyoei_date=20170209&gekijyo_cd=0761&screen_cd=10&sakuhin_cd=014183&pf_no=5&fnc=1&pageid=2000J01&enter_kbn=']
 
     # success url
     # https://hlo.tohotheater.jp/net/ticket/076/TNPI2040J03.do?site_cd=076&jyoei_date=20170209&gekijyo_cd=0761&screen_cd=10&sakuhin_cd=014183&pf_no=5&fnc=1&pageid=2000J01&enter_kbn=
@@ -16,10 +16,13 @@ class QuotesSpider(scrapy.Spider):
     # request data
 
     def parse(self, response):
-        empty_seat_count = len(response.css('[alt~="空席(選択可)"]'))
-        booked_seat_count = len(response.css('[alt~="購入済(選択不可)"]'))
-        total_seat_count = empty_seat_count + booked_seat_count
+        #empty_seat_count = len(response.css('[alt~="空席(選択可)"]'))
+        #booked_seat_count = len(response.css('[alt~="購入済(選択不可)"]'))
+        #total_seat_count = empty_seat_count + booked_seat_count
 
+        #yield {
+        #    'result': str(booked_seat_count)+'/'+str(total_seat_count)
+        #}
         yield {
-            'result': str(booked_seat_count)+'/'+str(total_seat_count)
+            'result': len(response.css('div.schedule-item'))
         }
